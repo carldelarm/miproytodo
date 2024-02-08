e7d8a4f7850ee2a8b28d6e78f4d7fd535f99043a
INDRA\crosa <crosa@indracompany.com>
2024-02-08 07:13:08 -0500

    Se  suben cambios relacionados con la adición y eliminación de tareas

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
 
 interface Props {
-  taskName: string
+  id:number,
+  taskName: string,
+  tasksList:any[],
+  setTasksList:any
 }
 
-const Task = ({taskName}:Props) => {
+const Task = ({id,taskName,tasksList,setTasksList}:Props) => {
+
+  const onDeleteTask = (event:any) => {
+    const btnSelected = event.target.id;
+    const data = btnSelected.split('_');
+    const idToDelete = data[2];
+    const result = tasksList.filter((task:any) => task.id != idToDelete);
+    setTasksList(result);
+  }
+
   return (
-    <>
-        <section className='caja-tarea'>
-            <div style={{width:'10%'}}>
-              <input type="checkbox" name="" id="" />
-            </div>
-            <div style={{width:'60%', textAlign:'left'}}>
-              <label>{taskName}</label>
-            </div>
-            <div style={{width:'30%', textAlign:'right'}}>
-              <input type="button" value="Remove" className='btn btn-danger btn-sm'/>
-            </div>
-        </section>
-    </>
+    <section className='caja-tarea'>
+        <div style={{width:'10%'}}>
+          <input type="checkbox" name={`task_${id}`} id={`task_${id}`} />
+        </div>
+        <div style={{width:'60%', textAlign:'left'}}>
+          <label>{taskName}</label>
+        </div>
+        <div style={{width:'30%', textAlign:'right'}}>
+          <button className='btn btn-danger btn-sm' 
+            name={`btn_remove_${id}`} id={`btn_remove_${id}`}
+            onClick={onDeleteTask}>Remove</button>
+        </div>
+    </section>
   )
 }
 
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,27 +1,98 @@
 //import React from 'react';
+import { useEffect, useState } from 'react';
 import Task from './Task';
 
 const Todo = () => {
+
+  const [contador,setContador] = useState(0);
+  const [tasksList, setTasksList] = useState([]);
+  const [description, setDescription] = useState("");
+
+  const getListTasks = async () => {
+    console.log('Iniciando mi aplicación TODO');
+  }
+
+  useEffect(() => {
+    getListTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onAddNewTask = (event:any) => {
+    event.preventDefault();
+
+    if(description.trim() === ''){
+      alert(`Por favor, ingrese una tarea valida`);
+      setDescription('');
+      return;
+    }
+
+    let existe = false;
+
+    tasksList.forEach((element) => {
+      const { name } = element;
+      const itemName:string = name;
+      if(description.toLowerCase().trim() === itemName.toLowerCase().trim()) existe = true;
+    });
+
+    if(existe){
+      alert(`La tarea ya existe, por favor ingrese una nueva`);
+      setDescription('');
+      return;
+    }
+
+    const newTask = {
+      id: contador,
+      name: `${description}`,
+      done: false
+    }
+
+    const tasksListToUpdate:any = [...tasksList, newTask];
+    setTasksList(tasksListToUpdate);
+    setContador(contador+1);
+    setDescription("");
+
+  }  
+
+  const onInputChange = (event:any) => {
+    const {target} = event;
+    const {value} = target;
+    setDescription(value);
+  }
+
   return (
-    <div className='contenedor'>
-      <h2>Mi Proyecto TODO</h2>
+      <div className='contenedor'>
+        <h2>Mi Proyecto TODO</h2>
 
-      <section id="busqueda">
-        <div>
-          <input type="text" name='taskname' id='taskname' placeholder='Ingrese una nueva tarea...'/>&nbsp;&nbsp;
-          <input type="button" value="Save" className='btn btn-primary' />&nbsp;&nbsp;
-          {/* <input type="reset" value="Limpiar" className='btn btn-secondary' /> */}
-        </div>
-      </section>
-      <br />
+        <section id="busqueda">
+          <div>
+            <form>
+              <input 
+                  type="text"
+                  name='taskname' 
+                  id='taskname' 
+                  placeholder='Ingrese una nueva tarea...'
+                  value={ description }
+                  onChange={ onInputChange }
+              />&nbsp;&nbsp;
+              <button type='submit' className='btn btn-primary' onClick={onAddNewTask}>Add Task</button>&nbsp;&nbsp;
+            </form>
+          </div>
+        </section>
+        <br />
 
-      <Task taskName={'Tarea a realizar # 1'} />
-      <Task taskName={'Tarea a realizar # 2'} />
-      <Task taskName={'Tarea a realizar # 3'} />
-      <Task taskName={'Tarea a realizar # 4'} />
-      <Task taskName={'Tarea a realizar # 5'} />
+        {
+          tasksList.map( (dataTask:any) => (
+            <Task key={dataTask.id} 
+                  id={dataTask.id} 
+                  taskName={`${dataTask.name}`} 
+                  tasksList={tasksList}
+                  setTasksList={setTasksList}
+             />
+          ))
+        }
 
-    </div>
+      </div>
+
   )
 }
 
